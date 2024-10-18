912b48e658b4c4e3190eea53e1f603f288bfe560
Deepanshukumar027 <tdeepanshu640@gmail.com>
2024-10-18 12:42:22 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,38 @@
+const endDate = "3 October 2024 12:00 AM"
+
+document.getElementById("end-date").innerText = endDate;
+const inputs = document.querySelectorAll("input")
+
+// const clock =()=> {
+
+// }
+
+function clock() {
+    const end = new Date(endDate)
+    const now = new Date()
+    const diff = (end-now) / 1000;
+
+
+    if(diff < 0) return;
+
+    // convert into days;
+    inputs[0].value = Math.floor (diff / 3600 / 24);
+    // convert into hours;
+    inputs[1].value = Math.floor(diff / 3600) % 24;
+    // convert into Minutes;
+    inputs[2].value = Math.floor(diff / 60) % 60;
+    inputs[3].value = Math.floor(diff) % 60;
+} 
+
+// initial call
+clock()
+
+
+
+
+setInterval(
+    () => {
+        clock()
+    },
+    1000
+)
